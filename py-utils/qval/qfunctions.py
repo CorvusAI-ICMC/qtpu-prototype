913a8a4db6e3494adef1d99be905920da55bf788
Petroncini <caiopetroncini@gmail.com>
2024-07-31 18:13:18 -0300

    fixed qfunc

--- py-utils/qval/qfunctions.py
+++ py-utils/qval/qfunctions.py
@@ -2,7 +2,6 @@ import math
 from typing import Callable
 
 
-
 class qval(str):
     def __new__(cls, value, n):
         # Ensure that the value contains only '0' and '1'
@@ -177,7 +176,7 @@ def dequantize(q: str, n: int = None) -> float:
 
     return sign * val
 
-
+ 
 def qToInt(q : str) -> int:
     if len(q) < 1:
         raise ValueError('The number of bits must be an integer greater than zero.')
@@ -202,21 +201,19 @@ def qFromInt(x : int, n : int) -> str:
     result = sign_bit + bin(abs_val)[2:].zfill(n - 1)
 
     return result if len(result) <= n else qMax(n)
-         
 
-def qAdd(a : str, b : str) -> str:
-    if len(a) != len(b): 
+
+def qAdd(a: str, b: str) -> str:
+    if len(a) != len(b):
         raise ValueError('Both quantized values must have the same number of bits.')
     if len(a) < 1:
         raise ValueError('The number of bits must be an integer greater than zero.')
-    
-    maxInt = qMax_i(len(a) + 1)
-    sum = qToInt(a) + qToInt(b)
-    if(sum > maxInt):
-        sum = maxInt
-    
-    
-    return qFromInt(sum, len(a) + 1)
+
+    # maxInt = qMax_i(len(a))  # Max int for the given number of bits
+    qsum = qToInt(a) + qToInt(b)
+    # qsum = max(-maxInt, min(qsum, maxInt))
+
+    return qFromInt(qsum, len(a))  # Ensure the result fits into the original bit length
 
 
 def qSub(a : str, b : str) -> str:
@@ -239,9 +236,14 @@ def qMul(a : str, b : str) -> str:
         raise ValueError('Both quantized values must have the same number of bits.')
     if len(a) < 1:
         raise ValueError('The number of bits must be an integer greater than zero.')
-    
+
+    # maxInt = qMax_i(len(a))
+    # mul = qToInt(a) * qToInt(b)
+    # mul = max(-maxInt, min(mul, maxInt))
+    # 
+    # return qFromInt(mul, 1 + (len(a) - 1) * 2)
     val = qToInt(a) * qToInt(b)
-    return qFromInt(val, 1 + (len(a) - 1) * 2)
+    return qfit(qFromInt(val, 1 + (len(a) - 1) * 2), len(a))
 
 def qFun(f : Callable[[float], float]) -> str:
     return lambda q: quantize(f(dequantize(q)), len(q))
